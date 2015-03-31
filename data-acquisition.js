6aec6707ac8ea9a0aac47d1d792f7db9ff93addd
saxsir <saxsir.256@gmail.com>
2015-03-31 14:21:03 +0900

    [modify] URLのリストをログファイルから直接抽出するように変更

--- data-acquisition.js
+++ data-acquisition.js
@@ -1,6 +1,16 @@
 /*
  * 読み込んだURL一覧全てのタイトルを取得して出力するスクリプト
  */
+
+// ログファイルからURLのみを抽出
+var fs = require('fs');
+//NOTE: Nodeのfsモジュールとは違う同期API
+//cf. http://phantomjs.org/api/fs/method/read.html
+var lines = fs.read('log/search-result.latest.txt')
+    .split('\n');
+lines.pop();
+urls = lines.splice(3);
+
 var casper = require('casper').create({
   verbose: true,
   logLebel: 'warning',
@@ -9,9 +19,7 @@ var casper = require('casper').create({
     height: 1600
   }
 });
-
 casper.start();
-urls = require('target.json');
 
 casper.each(urls, function(casper, url, i) {
   return this.thenOpen(url, function() {
